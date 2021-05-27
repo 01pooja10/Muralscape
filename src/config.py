d1e1d25ab90395416bcd621520ea53066d4f9535
01pooja10 <66198904+01pooja10@users.noreply.github.com>
2021-05-27 20:22:45 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,9 +1,13 @@
+import torch
+
 path = r'C:\Users\Pooja\Documents\ML_projects\Muralscape\model'
 im_size = 256
-device = 'cuda'
-im_path = r''
-style_path = r''
-epochs = 200
+device = torch.device('cuda')
+im_path = r'C:\Users\Pooja\Documents\ML_projects\Muralscape\assets\jimi.jpeg'
+style_path = r'C:\Users\Pooja\Documents\ML_projects\Muralscape\assets\psy.jpg'
+out_path = r'C:\Users\Pooja\Documents\ML_projects\Muralscape\samples'
+model_path = r'C:\Users\Pooja\Documents\ML_projects\Muralscape\model'
+epochs = 1000
 a = 1
 b = 0.01
 lr = 0.001
