33c249ff69ca99b5e9120d22a7621bed2386f5f1
01pooja10 <66198904+01pooja10@users.noreply.github.com>
2021-05-29 20:56:59 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -6,8 +6,8 @@ set hyperparameter values
 path = r'C:\Users\Pooja\Documents\ML_projects\Muralscape\model'
 im_size = 256
 device = torch.device('cuda')
-im_path = r'C:\Users\Pooja\Documents\ML_projects\Muralscape\assets\iceland.jpg'
-style_path = r'C:\Users\Pooja\Documents\ML_projects\Muralscape\assets\vwfc.jpg'
+im_path = r'C:\Users\Pooja\Documents\ML_projects\Muralscape\assets\jimi.jpeg'
+style_path = r'C:\Users\Pooja\Documents\ML_projects\Muralscape\assets\psy.jpg'
 out_path = r'C:\Users\Pooja\Documents\ML_projects\Muralscape\samples'
 model_path = r'C:\Users\Pooja\Documents\ML_projects\Muralscape\model'
 epochs = 4000
